9ddabe392e531d2019f2cf83ca919edf9b4476e0
Aliffahrezi9 <aliffahrezi9@gmail.com>
2025-08-19 20:20:25 +0700

    delete file in migration and seeder who not required

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,17 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+    
 
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
-            KategoriPertanyaanSeeder::class,
-            PertanyaanSeeder::class
         ]);
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+       
     }
 }
