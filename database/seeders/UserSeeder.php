ae96047f4a0abff7323c68674ab5ffa313b7c661
Aliffahrezi9 <aliffahrezi9@gmail.com>
2025-08-24 08:52:02 +0700

    edit seeder user admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -27,5 +27,41 @@ class UserSeeder extends Seeder
                 'role_id' => '1'
             ]);
         }
+        if(!User::where('email', 'admin_farid@gmail.com')->first()){
+            // id, username, password, email, nama_depan, nama_belakang, status, role_id,
+            User::create([
+                'username' => 'admin_farid',
+                'password' => bcrypt('rahasia'),
+                'email' => 'admin_farid@gmail.com',
+                'nama_depan' => 'Admin',
+                'nama_belakang' => 'Farid',
+                'status' => '1',
+                'role_id' => '1'
+            ]);
+        }
+        if(!User::where('email', 'admin_roofi@gmail.com')->first()){
+            // id, username, password, email, nama_depan, nama_belakang, status, role_id,
+            User::create([
+                'username' => 'admin_roofi',
+                'password' => bcrypt('rahasia'),
+                'email' => 'admin_roofi@gmail.com',
+                'nama_depan' => 'Admin',
+                'nama_belakang' => 'Roofi',
+                'status' => '1',
+                'role_id' => '1'
+            ]);
+        }
+        if(!User::where('email', 'admin_alif@gmail.com')->first()){
+            // id, username, password, email, nama_depan, nama_belakang, status, role_id,
+            User::create([
+                'username' => 'admin_alif',
+                'password' => bcrypt('rahasia'),
+                'email' => 'admin_alif@gmail.com',
+                'nama_depan' => 'Admin',
+                'nama_belakang' => 'Alif',
+                'status' => '1',
+                'role_id' => '1'
+            ]);
+        }
     }
 }
