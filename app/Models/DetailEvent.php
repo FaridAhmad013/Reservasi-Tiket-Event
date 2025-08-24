902723574f3a53eec20bd090b7d1da4da716941a
Aliffahrezi9 <aliffahrezi9@gmail.com>
2025-08-24 08:37:37 +0700

    edit model

--- app/Models/DetailEvent.php
+++ app/Models/DetailEvent.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class DetailEventModel extends Model
+class DetailEvent extends Model
 {
     protected $fillable = [
         'event_id',
--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class TransaksiModel extends Model
+class Transaksi extends Model
 {
     protected $fillable = [
         'detail_event_id',
